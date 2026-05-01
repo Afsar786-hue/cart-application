65f30d2999586d6e822bfc69dac50eb3c2f65b01
Afsar786-hue <muhammedafsar6778@gmail.com>
2026-05-01 15:09:37 +0530

    Create cartController.js with cart management functions

---
+++ server/controllers/cartController.js
@@ -0,0 +1,34 @@
+// Cart Controller for managing shopping cart operations
+
+// Function to get the cart
+async function getCart(req, res) {
+    // Logic to get cart
+}
+
+// Function to add item to cart
+async function addToCart(req, res) {
+    // Logic to add item to cart
+}
+
+// Function to remove item from cart
+async function removeFromCart(req, res) {
+    // Logic to remove item from cart
+}
+
+// Function to update cart item
+async function updateCartItem(req, res) {
+    // Logic to update item in cart
+}
+
+// Function to clear the cart
+async function clearCart(req, res) {
+    // Logic to clear the cart
+}
+
+module.exports = {
+    getCart,
+    addToCart,
+    removeFromCart,
+    updateCartItem,
+    clearCart
+};
